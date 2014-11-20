8550cca1e1f4b18735530ed7e396d3ac07a21ea7
zorroboy80 <zorroboy80@gmail.com>
2014-11-20 20:51:41 +0100

    refactoring authentication script

--- first_test.rb
+++ first_test.rb
@@ -2,37 +2,5 @@ require 'rubygems'
 require 'oauth'
 require 'yaml'
 
-# Import codes etc.
-config = YAML.load_file("config.yaml")
-
-# Change the following values to those provided on dev.twitter.com
-# The consumer key identifies the application making the request.
-# The access token identifies the user making the request.
-consumer_key = OAuth::Consumer.new(
-    config["twitter"]["consumer_key1"],
-    config["twitter"]["consumer_key2"])
-access_token = OAuth::Token.new(
-    config["twitter"]["access_token1"],
-    config["twitter"]["access_token2"])
-
-	
-# All requests will be sent to this server.
-baseurl = "https://api.twitter.com"
-
-# The verify credentials endpoint returns a 200 status if
-# the request is signed correctly.
-address = URI("#{baseurl}/1.1/account/verify_credentials.json")
-
-# Set up Net::HTTP to use SSL, which is required by Twitter.
-http = Net::HTTP.new address.host, address.port
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-# Build the request and authorize it with OAuth.
-request = Net::HTTP::Get.new address.request_uri
-request.oauth! http, consumer_key, access_token
-
-# Issue the request and return the response.
-http.start
-response = http.request request
-puts "The response status was #{response.code}"
+# Other scripts
+require_relative 'authenticate.rb'
