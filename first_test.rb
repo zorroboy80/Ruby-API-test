9f5ed6fc664f19307183d295ff6fa80b4bcf7884
zorroboy80 <zorroboy80@gmail.com>
2014-11-20 21:29:23 +0100

    Posting to Twitter!

--- first_test.rb
+++ first_test.rb
@@ -55,3 +55,40 @@ def parse_user_response(response)
 end
 
 user = parse_user_response(response)
+
+
+
+# Time to post!
+
+baseurl = "https://api.twitter.com"
+path    = "/1.1/statuses/update.json"
+address = URI("#{baseurl}#{path}")
+request = Net::HTTP::Post.new address.request_uri
+request.set_form_data(
+  "status" => "Join my Collaboration Meeting Room using TelePresence: sip:901234567@example.org",
+)
+
+http             = Net::HTTP.new address.host, address.port
+http.use_ssl     = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+
+request.oauth! http, consumer_key, access_token
+http.start
+response = http.request request
+
+# Parse and print the Tweet if the response code was 200
+tweet = nil
+if response.code == '200' then
+  tweet = JSON.parse(response.body)
+  puts "Successfully sent #{tweet["text"]}"
+else
+  puts "Could not send the Tweet! " +
+  "Code:#{response.code} Body:#{response.body}"
+end
+
+
+
+
+
+
